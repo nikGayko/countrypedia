8036ae40a4714585da3fbbafb8c6014820085c14
NIkita Gayko <nik.gayko@gmail.com>
2017-06-22 03:34:04 +0300

    add google map

---
+++ Countrypedia/app/src/main/java/com/example/nick/countrypedia/view/MapsActivity.java
@@ -0,0 +1,33 @@
+package com.example.nick.countrypedia.view;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.nick.countrypedia.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
